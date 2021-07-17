b652d2f11d6d5375008a2a84266efd5d7c805c09
Sergey-F84 <legal77@bk.ru>
2021-07-17 13:17:23 +0900

    create: connecting Yandex maps to the block map

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+});
+type="text/javascript">
+    ymaps.ready(init);
+    function init(){
+        var myMap = new ymaps.Map("map", {
+            center: [7.890759, 98.294690],
+            zoom: 17
+        });
+    }
